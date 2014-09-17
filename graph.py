9ecff18e3b6bc607ee5234d6f978a5af5c722651
Travis Dean <travis.j.dean@gmail.com>
2014-09-17 19:50:08 -0400

    Code all working. Needs coverage and docs.

--- graph.py
+++ graph.py
@@ -1,9 +1,9 @@
-__author__ = 'Travis'
+__author__ = 'tjd2qj'
 
 
 class Graph(object):
-    def __init__(self, dict={}):
-        self.nodes = dict.copy()
+    def __init__(self, d={}):
+        self.nodes = d.copy() or {}
 
     def __len__(self):
         return len(self.nodes)
@@ -13,9 +13,7 @@ class Graph(object):
 
     def __getitem__(self, item):
         """
-
-
-        :rtype : object
+        :rtype : list
         :param item:
         :return:
         """
@@ -25,38 +23,56 @@ class Graph(object):
         if node in self.nodes:
             return self.nodes[node]
 
-
-
-
-
     def is_adjacent(self, node1, node2):
         if not node1 in self.nodes: return False
         if node2 in self.nodes[node1]: return True
         return False
 
-
     def num_nodes(self):
         return len(self.nodes)
-        pass
-
 
 
     def __iter__(self):
         return iter(self)
-        pass
 
 
     def __contains__(self, node):
         return node in self.nodes
 
 
-
+    # noinspection PyPep8Naming
     def addNode(self, node):
-        pass
+        if node in self.nodes: return False
+        self.nodes[node] = []
+        return True
+
+    def del_node(self, node):
+        ret = False
+        for v in self.nodes.values():
+            if node in v:
+                v.remove(node)
+                ret = True
+        if node in self.nodes.copy():
+            del self.nodes[node]
+            ret = True
+        return ret
 
 
     def link_nodes(self, node1, node2):
-        pass
+        if node1 not in self.nodes or node2 in self.nodes[node1] or node2 not in self.nodes or node1 in self.nodes[
+            node2] or node1 is node2: return False
+
+        self.nodes[node1] += node2
+        self.nodes[node2] += node1
+        return True
+
+    def unlink_nodes(self, node1, node2):
+        if self.nodes.__contains__(node1 and node2):
+            if self.is_adjacent(node1, node2) and self.is_adjacent(node2, node1):
+                self.nodes[node1].remove(node2)
+                self.nodes[node2].remove(node1)
+                return True
+        return False
 
 
-g1 = Graph({'A': ['B', 'D'], 'B': ['A', 'D', 'C'], 'C': ['B'], 'D': ['A', 'B'], 'E': []})
+g2 = Graph({'A': ['B', 'D'], 'B': ['A', 'D', 'C'], 'C': ['B'], 'D': ['A', 'B'], 'E': []})
--- test_graph.py
+++ test_graph.py
@@ -1,23 +1,25 @@
-from unittest import TestCase
-
-__author__ = 'Travis'
+__author__ = 'tjd2qj'
 import unittest
+
 from graph import Graph
 
-
+# noinspection PyPep8Naming
 class TestGraph(unittest.TestCase):
     def setUp(self):
         self.emp = Graph()
         self.big = Graph({'A': ['B', 'D'], 'B': ['A', 'D', 'C'], 'C': ['B'], 'D': ['A', 'B'], 'E': []})
         self.a = Graph({'A': []})
+        self.ac = Graph({'A': [], 'C': []})
         self.aB = Graph({'A': ['B']})
-        self.aBD = Graph({'A': ['B','D']})
+        self.aBD = Graph({'A': ['B', 'D']})
         self.bC = Graph({'B': ['C']})
         self.aBbA = Graph({'A': ['B'], 'B': ['A']})
         self.aBbAc = Graph({'A': ['B'], 'B': ['A'], 'C': []})
-        self.aBbAc = Graph({'A': ['B'], 'B': ['A'], 'C': ['A','B']})
+        self.aBbAcAB = Graph({'A': ['B'], 'B': ['A'], 'C': ['A', 'B']})
+        self.aBCbAcA = Graph({'A': ['B', 'C'], 'B': ['A'], 'C': ['A']})
+        self.aBbC = Graph({'A': ['B'], 'B': ['C']})
 
-#region get_adj_testing
+    # region get_adj_testing
     def test_get_adjlist_q1(self):
         g = self.a
         v = 'A'
@@ -28,7 +30,7 @@ class TestGraph(unittest.TestCase):
     def test_get_adjlist_q2(self):
         g = self.aBD
         v = 'A'
-        exp = ['B','D']
+        exp = ['B', 'D']
         res = g.get_adjlist(v)
         self.assertEqual(res, exp)
 
@@ -38,26 +40,29 @@ class TestGraph(unittest.TestCase):
         exp = None
         res = g.get_adjlist(v)
         self.assertEqual(res, exp)
-#endregion
-#region is_adj_test
+    # endregion
+
+    # region is_adj_test
     def test_is_adjacent_q4(self):
         g = self.aBD
-        v = ('A','B')
+        v = ('A', 'B')
         res = g.is_adjacent(*v)
         self.assertTrue(res)
 
     def test_is_adjacent_q5(self):
         g = self.aBD
-        v = ('B','A')
+        v = ('B', 'A')
         res = g.is_adjacent(*v)
         self.assertFalse(res)
 
     def test_is_adjacent_q6(self):
         g = self.emp
-        v = ('B','A')
+        v = ('B', 'A')
         res = g.is_adjacent(*v)
         self.assertFalse(res)
 
+    # endregion
+    #region num_nodes
     def test_num_nodes_q7(self):
         g = self.emp
         res = g.num_nodes()
@@ -69,7 +74,8 @@ class TestGraph(unittest.TestCase):
         res = g.num_nodes()
         self.assertEqual(res, 5)
 
-
+    #endregion
+    #region contains
     def test_contains_q9(self):
         g = self.aBD
         v = 'A'
@@ -82,16 +88,21 @@ class TestGraph(unittest.TestCase):
         res = g.__contains__(v)
         self.assertFalse(res)
 
+    #endregion
+    #region len
     def test_len_q11(self):
         g = self.emp
         res = len(g)
         self.assertEqual(res, 0)
 
     def test_len_q12(self):
-        g = self.aBD
+        g = self.aBbAc
         res = len(g)
         self.assertEqual(res, 3)
 
+    #endregion
+
+    #region add_node
     def test_addNode_q13(self):
         g = self.emp
         v = 'A'
@@ -104,93 +115,93 @@ class TestGraph(unittest.TestCase):
         res = g.addNode(v)
         self.assertFalse(res)
 
-    def test_link_nodes_q15(self):
-        g = self.aBbA
-        v = ('A','B')
-        res = g.link_nodes(*v)
-        self.assertFalse(res)
+    def test_addNode_q15(self):
+        g = self.bC
+        v = 'A'
+        res = g.addNode(v)
+        self.assertTrue(res)
+        self.assertEqual(g.get_adjlist('A'), [])
 
+    #endregion
+
+    #region link_nodes
     def test_link_nodes_q16(self):
+        """Checking for already adj links"""
         g = self.aBbA
-        v = ('A','B')
+        v = ('A', 'B')
         res = g.link_nodes(*v)
         self.assertFalse(res)
 
     def test_link_nodes_q17(self):
-        g = self.aBbA
-        v = ('A','B')
+        """Test to make sure that the nodes were added correctly."""
+        g = self.aBbAc
+        v = ('A', 'C')
         res = g.link_nodes(*v)
-        self.assertFalse(res)
+        self.assertTrue(res)
+        self.assertEqual(g.nodes, self.aBCbAcA.nodes)
 
     def test_link_nodes_q18(self):
         g = self.aBbA
-        v = ('A','B')
+        v = ('A', 'A')
         res = g.link_nodes(*v)
         self.assertFalse(res)
 
     def test_link_nodes_q19(self):
-        g = self.aBbA
-        v = ('A','B')
+        """Test for nodes not in the graph to fail"""
+        g = self.ac
+        v = ('A', 'B')
         res = g.link_nodes(*v)
         self.assertFalse(res)
 
-    def test_contains_q10(self):
-        g = self.aBD
-        v = 'B'
-        res = g.__contains__(v)
+    #endregion
+
+    #region unlink_nodes
+    def test_unlink_nodes_q20(self):
+        """Unlinking nodes not intially adj returns false.
+
+
+        """
+        g = self.aBbC
+        v = ('A', 'B')
+        res = g.unlink_nodes(*v)
         self.assertFalse(res)
 
-    def test_contains_q10(self):
-        g = self.aBD
-        v = 'B'
-        res = g.__contains__(v)
+    def test_unlink_nodes_q21(self):
+        """Insure that unlinking nodes not in the graph returns False.
+
+
+        """
+        g = self.aB
+        v = ('A', 'B')
+        res = g.unlink_nodes(*v)
         self.assertFalse(res)
 
-    def test_contains_q10(self):
-        g = self.aBD
-        v = 'B'
-        res = g.__contains__(v)
+    def test_unlink_nodes_q22(self):
+        """Require that unlinking nodes actually unlinkes them, and not others.
+
+
+        """
+        g = self.aBbA
+        v = ('A', 'B')
+        res = g.unlink_nodes(*v)
+        self.assertTrue(res)
+
+    #endregion
+
+    #region del_node
+    def test_del_node_q23(self):
+        g = self.aB
+        v = 'C'
+        res = g.del_node(v)
         self.assertFalse(res)
 
-    def test_contains_q10(self):
-        g = self.aBD
+    def test_del_node_q24(self):
+        g = self.aBbAcAB
         v = 'B'
-        res = g.__contains__(v)
-        self.assertFalse(res)
-
-    def test_contains_q10(self):
-        g = self.aBD
-        v = 'B'
-        res = g.__contains__(v)
-        self.assertFalse(res)
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+        res = g.del_node(v)
+        self.assertTrue(res)
+        self.assertDictEqual(g.nodes, {'A': [], 'C': ['A']})
+    #endregion
 
 
 if __name__ == "__main__":
